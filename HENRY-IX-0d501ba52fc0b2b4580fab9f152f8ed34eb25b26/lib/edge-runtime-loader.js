b305d8fa73573a9868db8c341fecb16575796586
henryixdj-art <henryixdj@gmail.com>
2026-07-17 18:13:36 +0100

    chore: migrate from next-on-pages to OpenNext Cloudflare Workers

--- HENRY-IX-0d501ba52fc0b2b4580fab9f152f8ed34eb25b26/lib/edge-runtime-loader.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(source) {
-  if (!source.includes("export const runtime = 'edge'") && !source.includes("export const runtime='edge'")) {
-    return source + "\nexport const runtime = 'edge';";
-  }
-  return source;
-};
