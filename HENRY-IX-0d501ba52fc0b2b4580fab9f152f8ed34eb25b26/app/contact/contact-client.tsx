bdf1ffd616dac760907ce8ed1d7fe5621b09ebab
HENRY IX <henryixdj@gmail.com>
2026-07-18 14:23:55 +0100

    feat: add contact client page with contact form component

--- HENRY-IX-0d501ba52fc0b2b4580fab9f152f8ed34eb25b26/app/contact/contact-client.tsx
+++ HENRY-IX-0d501ba52fc0b2b4580fab9f152f8ed34eb25b26/app/contact/contact-client.tsx
@@ -1,13 +1,12 @@
 'use client';
 
 import PageShell from '@/components/PageShell';
-import { ContactForm, SocialLinks } from '@/components/ContentSections';
+import { ContactForm } from '@/components/ContentSections';
 
 export default function ContactClient() {
   return (
     <PageShell>
       <ContactForm isDepth={true} />
-      <SocialLinks isDepth={true} />
     </PageShell>
   );
 }
